8cd025a410b5716e479339cc82974d9a233995b5
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-01-08 15:57:53 -0400

    solucion variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 export const pgdb = {
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_HOST: process.env.DB_HOST,
-    DB_DATABASE: process.env.DB_DATABASE,
-    DB_PORT: process.env.DB_PORT || 5432,
-    // PORT: process.env.PORT
+// Si no existe, usa los de desarrollo
+DB_USER: process.env.DB_USER || 'postgres',
+DB_PASSWORD: process.env.DB_PASSWORD || '1234',
+DB_HOST: process.env.DB_HOST || 'localhost',
+DB_DATABASE: process.env.DB_DATABASE || 'delivery',
+DB_PORT: process.env.DB_PORT || 5432,
+
+// Estas sí son necesarias siempre
+JWT_SECRET: process.env.JWT_SECRET || 'palabrasecreta'
 }
