8b5ac62aae35106b5117c89ce8e0a9683c8e53a2
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-01-08 15:31:23 -0400

    solucionando cor

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ export const pgdb = {
     DB_HOST: process.env.DB_HOST,
     DB_DATABASE: process.env.DB_DATABASE,
     DB_PORT: process.env.DB_PORT || 5432,
-    PORT: process.env.PORT
+    // PORT: process.env.PORT
 }
