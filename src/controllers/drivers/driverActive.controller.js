45f9248d689d8e5a20a7f17d87a94f48789b38a9
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-04-17 15:49:03 -0400

    activar conductor

---
+++ src/controllers/drivers/driverActive.controller.js
@@ -0,0 +1,45 @@
+import { pool } from '../../db.js';
+
+export const activateDriver = async (req, res) => {
+    const { usuario_id } = req.body;
+
+    if (!usuario_id) {
+        return res.status(400).json({ 
+            success: false, 
+            error: 'El ID de usuario es requerido.' 
+        });
+    }
+
+    try {
+        // Ejecutamos la actualización
+        const query = `
+            UPDATE repartidores 
+            SET is_active = 'activo', 
+                is_available = true 
+            WHERE usuario_id = $1 
+            RETURNING *;
+        `;
+        
+        const result = await pool.query(query, [usuario_id]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).json({ 
+                success: false, 
+                error: 'No se encontró el registro del repartidor.' 
+            });
+        }
+
+        res.json({
+            success: true,
+            message: 'Conductor activado con éxito',
+            data: result.rows[0]
+        });
+
+    } catch (error) {
+        console.error('Error al activar conductor:', error);
+        res.status(500).json({ 
+            success: false, 
+            error: 'Error interno al intentar activar al conductor.' 
+        });
+    }
+};
--- src/routes/driver/driverRegisterModal.route.js
+++ src/routes/driver/driverRegisterModal.route.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { verifyToken } from '../middlewares/verifyToken.js'; // 👈 Importar el middleware
 import { registerDriverInterview } from "../../controllers/drivers/driverRegisterModal.controller.js";
 import { suspendDriver } from "../../controllers/drivers/driverSuspend.controller.js";
+import { activateDriver } from "../../controllers/drivers/driverActive.controller.js";
 
 const routerDriverRegisterModal = Router();
 
 routerDriverRegisterModal.post('/driver/driver-register-modal',verifyToken, registerDriverInterview );
 routerDriverRegisterModal.put('/driver/suspend-driver', verifyToken, suspendDriver);
+routerDriverRegisterModal.put('/driver/activate-driver', verifyToken, activateDriver);
 
 export default routerDriverRegisterModal;   
