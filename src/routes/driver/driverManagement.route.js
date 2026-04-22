304a576f9b974ae8bb9b85c107defd27206fe502
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-04-22 10:48:29 -0400

    integrar socket.io para notificacion conductores 5

--- src/routes/driver/driverManagement.route.js
+++ src/routes/driver/driverManagement.route.js
@@ -3,7 +3,7 @@ import {
     toggleAvailability, 
     getCurrentOrder, 
     completeOrder 
-} from '../../controllers/ManagementDriver.controller.js';
+} from '../../controllers/drivers/managementDriver.controller.js';
 import { verifyToken } from '../middlewares/auth.middleware.js'; // Tu middleware de JWT
 
 const routerDriverManagement = Router();
