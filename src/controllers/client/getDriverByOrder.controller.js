c7cdd306e86d13851bb413b190c75cc7094f32f4
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-04-26 22:58:49 -0400

    ruta modal vistar driver pedido 3

--- src/controllers/client/getDriverByOrder.controller.js
+++ src/controllers/client/getDriverByOrder.controller.js
@@ -1,5 +1,5 @@
 // En tu archivo de rutas o controlador de clientes
-import pool from '../db.js';
+import { pool } from "../../db.js";
 export const getDriverByOrder = async (req, res) => {
     const { pedidoId } = req.params;
 
