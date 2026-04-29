3a72d72af3a9101ab31dc634c0297e29488edf13
Ramon Gonzalez <ramongonzalez101@gmail.com>
2026-04-28 22:24:21 -0400

    lista conductores activos 6

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ import routerDriverManagement from './routes/driver/driverManagement.route.js';
 import routerGetDriverByOrder from './routes/client/newOrderDriver.route.js';
 import routerAdminManagment from './routes/administrator/managment/managment.route.js'
 import routerActiveVehicleTypes from './routes/client/clienteActiveVehicleTypes.js';
-import routerAvailableDrivers from './routes/administrator/managment/driver/AvailableDrivers.route.js';
+import routerAvailableDrivers from './routes/administrator/managment/driver/availableDrivers.route.js';
 
 const app = express();
 const httpServer = createServer(app);
